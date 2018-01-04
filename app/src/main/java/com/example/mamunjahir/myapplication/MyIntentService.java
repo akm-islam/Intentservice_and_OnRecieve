40a32cdd346b0615487868218d3ea82519b0a6bd
Mamunjahir <Mamunjahir.com@gmail.com>
2018-01-04 15:34:44 -0500

    final2

--- app/src/main/java/com/example/mamunjahir/myapplication/MyIntentService.java
+++ app/src/main/java/com/example/mamunjahir/myapplication/MyIntentService.java
@@ -4,7 +4,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ResultReceiver;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class MyIntentService extends IntentService
--- app/src/main/java/com/example/mamunjahir/myapplication/ServiceDemo.java
+++ app/src/main/java/com/example/mamunjahir/myapplication/ServiceDemo.java
@@ -8,9 +8,6 @@ import android.os.ResultReceiver;
 import android.view.View;
 import android.widget.EditText;
 
-/**
- * Created by DELL on 05-04-2015.
- */
 public class ServiceDemo extends Activity
 {
     public static final int RESULT_CODE = 11;
@@ -31,23 +28,11 @@ public class ServiceDemo extends Activity
         ii.putExtra("receiver",r);
         startService(ii);
     }
-    public void stopMyService(View v)
-    {
-       // Intent ii=new Intent(this,MyIntentService.class);
-        //stopService(ii);
 
-    }
-    
+
+
     public class myreceiver extends  ResultReceiver
     {
-
-        /**
-         * Create a new ResultReceive to receive results.  Your
-         * {@link #onReceiveResult} method will be called from the thread running
-         * <var>handler</var> if given, or from an arbitrary thread if null.
-         *
-         * @param handler
-         */
         public myreceiver(Handler handler) {
             super(handler);
         }
